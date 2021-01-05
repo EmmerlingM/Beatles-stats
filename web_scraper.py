f1b7245e5ac1e82c95a37a23a28ac95f85f99637
Bartosz <bartoszvvegrzyn@gmail.com>
2021-01-05 22:13:18 +0100

    added csv_from_SongKeyFinder()

--- web_scraper.py
+++ web_scraper.py
@@ -2,23 +2,64 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-csv_file = open('songs_data.csv', 'w')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['title', 'key'])
 
-source = requests.get('https://inwhatkey.com/artist/74-the-beatles').text
-soup = BeautifulSoup(source, 'lxml')
+def csv_from_inwhatkey():
+    csv_file = open('songs_data1.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['title', 'key'])
 
-domain_name = 'https://inwhatkey.com'
+    source = requests.get('https://inwhatkey.com/artist/74-the-beatles').text
+    soup = BeautifulSoup(source, 'lxml')
 
-for a in soup.find_all('a', href=True):
-    if a['href'].startswith('/song'):
-        link = domain_name + a['href']
-        current_source = requests.get(link).text
-        current_soup = BeautifulSoup(current_source, 'lxml')
+    domain_name = 'https://inwhatkey.com'
 
-        key = current_soup.find("h3").text
-        title = current_soup.find("h1").text
-        csv_writer.writerow([title, key])
+    for a in soup.find_all('a', href=True):
+        if a['href'].startswith('/song'):
+            link = domain_name + a['href']
+            current_source = requests.get(link).text
+            current_soup = BeautifulSoup(current_source, 'lxml')
 
-csv_file.close()
+            key = current_soup.find("h3").text
+            title = current_soup.find("h1").text
+            csv_writer.writerow([title, key])
+
+    csv_file.close()
+
+
+def csv_from_SongKeyFinder():
+    csv_file = open('songs_data2.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['title', 'key'])
+
+    pages = [
+        "https://www.songkeyfinder.com/search?page=1&artist=The%20Beatles&song=",
+        "https://www.songkeyfinder.com/search?page=2&artist=The%20Beatles&song=",
+        "https://www.songkeyfinder.com/search?page=3&artist=The%20Beatles&song=",
+        "https://www.songkeyfinder.com/search?page=4&artist=The%20Beatles&song=",
+        "https://www.songkeyfinder.com/search?page=5&artist=The%20Beatles&song=",
+        "https://www.songkeyfinder.com/search?page=6&artist=The%20Beatles&song=",
+    ]
+    domain_name = 'https://www.songkeyfinder.com'
+
+    for i in range(6):
+
+        source = requests.get(pages[i]).text
+        soup = BeautifulSoup(source, 'lxml')
+
+        for a in soup.find_all('a', href=True):
+            if a['href'].startswith('/songs'):
+                link = domain_name + a['href']
+                current_source = requests.get(link).text
+                current_soup = BeautifulSoup(current_source, 'lxml')
+
+                key = current_soup.find("h3").text
+                title = current_soup.find("h2").text
+                key = key[9:-1]
+                title = title[:-14]
+
+                csv_writer.writerow([title, key])
+
+    csv_file.close()
+
+# csv_from_inwhatkey()
+# csv_from_SongKeyFinder()
